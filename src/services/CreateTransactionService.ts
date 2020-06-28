89fb970eed25a82432df286785bbe483654502ef
caio546 <caioom00@gmail.com>
2020-06-28 11:47:47 -0300

    Finish Challenge

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,74 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    const balance = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw new AppError('Transaction value is greater than current balance');
+    }
+
+    const categoryAlreadyExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (categoryAlreadyExists) {
+      const { id } = categoryAlreadyExists;
+
+      const newTransaction = transactionsRepository.create({
+        title,
+        value,
+        type,
+        category_id: id,
+      });
+
+      const savedTransaction = await transactionsRepository.save(
+        newTransaction,
+      );
+
+      return savedTransaction;
+    }
+
+    const newCategory = categoriesRepository.create({
+      title: category,
+    });
+
+    const savedCategory = await categoriesRepository.save(newCategory);
+
+    const { id } = savedCategory;
+
+    const newTransaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: id,
+    });
+
+    const savedTransaction = await transactionsRepository.save(newTransaction);
+
+    return savedTransaction;
   }
 }
 
